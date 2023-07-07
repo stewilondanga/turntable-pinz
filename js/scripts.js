32a4742dc83a96a50268fdb15137096b066f748d
Stewart <stilondanga@gmail.com>
2023-07-07 11:06:05 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/* var navigate = (function() {
+	$('.dd').toggle();
+	$('.dd_btn').click(function() {
+		var dataName = $(this).attr('data-name');
+		$('.dd').hide();
+		$('.' + dataName).toggle();
+	});
+})();
+
+const playButton = document.getElementById("play-button");
+const container = document.getElementById("button-container");
+
+playButton.addEventListener("click", () => {
+	if (container.getAttribute("data-playing") === "false") {
+		playButton.innerHTML = "&#10074;&#10074; Pause";
+		container.setAttribute("data-playing", "true");
+	} else {
+		playButton.innerHTML = "&#9658; Play";
+		container.setAttribute("data-playing", "false");
+	}
+});
+*/
