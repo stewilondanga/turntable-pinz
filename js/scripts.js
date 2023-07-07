d12ec54f4f1307fbd9409a77da6d877af3f5e5e2
Stewart <stilondanga@gmail.com>
2023-07-07 11:40:59 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
-/* var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
-})();
+ var navigate = (function() {
+   $('.dd').toggle();
+   $('.dd_btn').click(function() {
+     var dataName = $(this).attr('data-name');
+     $('.dd').hide();
+     $('.' + dataName).toggle();
+   });
+ })();
 
-const playButton = document.getElementById("play-button");
-const container = document.getElementById("button-container");
+ const playButton = document.getElementById("play-button");
+ const container = document.getElementById("button-container");
 
-playButton.addEventListener("click", () => {
-	if (container.getAttribute("data-playing") === "false") {
-		playButton.innerHTML = "&#10074;&#10074; Pause";
-		container.setAttribute("data-playing", "true");
-	} else {
-		playButton.innerHTML = "&#9658; Play";
-		container.setAttribute("data-playing", "false");
-	}
-});
-*/
+ playButton.addEventListener("click", () => {
+   if (container.getAttribute("data-playing") === "false") {
+     playButton.innerHTML = "&#10074;&#10074; Pause";
+     container.setAttribute("data-playing", "true");
+   } else {
+     playButton.innerHTML = "&#9658; Play";
+     container.setAttribute("data-playing", "false");
+   }
+ });
