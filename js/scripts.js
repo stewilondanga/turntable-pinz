eaabf8281c9b571029ef6fc12c2d3c540cdf6c65
Stewart <stilondanga@gmail.com>
2023-08-22 19:52:58 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,17 @@
      $('.' + dataName).toggle();
    });
  })();
- /*
-  const playButton = document.getElementById("play-button");
-  const container = document.getElementById("button-container");
 
-  playButton.addEventListener("click", () => {
-    if (container.getAttribute("data-playing") === "false") {
-      playButton.innerHTML = "&#10074;&#10074; Pause";
-      container.setAttribute("data-playing", "true");
-    } else {
-      playButton.innerHTML = "&#9658; Play";
-      container.setAttribute("data-playing", "false");
-    }
-  });
- */
+ const playButton = document.getElementById("play-button");
+ /*  const container = document.getElementById("button-container");
+
+   playButton.addEventListener("click", () => {
+     if (container.getAttribute("data-playing") === "false") {
+       playButton.innerHTML = "&#10074;&#10074; Pause";
+       container.setAttribute("data-playing", "true");
+     } else {
+       playButton.innerHTML = "&#9658; Play";
+       container.setAttribute("data-playing", "false");
+     }
+   });
+  */
