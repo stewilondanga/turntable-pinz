5b517d3e5fa8ab009c8e04069c10a3faf6d83fda
Stewart <stilondanga@gmail.com>
2023-08-22 19:53:32 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,15 @@
  })();
 
  const playButton = document.getElementById("play-button");
- /*  const container = document.getElementById("button-container");
-
-   playButton.addEventListener("click", () => {
-     if (container.getAttribute("data-playing") === "false") {
-       playButton.innerHTML = "&#10074;&#10074; Pause";
-       container.setAttribute("data-playing", "true");
-     } else {
-       playButton.innerHTML = "&#9658; Play";
-       container.setAttribute("data-playing", "false");
-     }
-   });
-  */
+ const container = document.getElementById("button-container");
+ /*
+    playButton.addEventListener("click", () => {
+      if (container.getAttribute("data-playing") === "false") {
+        playButton.innerHTML = "&#10074;&#10074; Pause";
+        container.setAttribute("data-playing", "true");
+      } else {
+        playButton.innerHTML = "&#9658; Play";
+        container.setAttribute("data-playing", "false");
+      }
+    });
+   */
