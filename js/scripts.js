114ffbb3bd96c1f182b91c1fc74c06f9115d1b20
Stewart <stilondanga@gmail.com>
2023-08-22 19:52:25 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/* var navigate = (function() {
+ var navigate = (function() {
    $('.dd').toggle();
    $('.dd_btn').click(function() {
      var dataName = $(this).attr('data-name');
@@ -6,17 +6,17 @@
      $('.' + dataName).toggle();
    });
  })();
+ /*
+  const playButton = document.getElementById("play-button");
+  const container = document.getElementById("button-container");
 
- const playButton = document.getElementById("play-button");
- const container = document.getElementById("button-container");
-
- playButton.addEventListener("click", () => {
-   if (container.getAttribute("data-playing") === "false") {
-     playButton.innerHTML = "&#10074;&#10074; Pause";
-     container.setAttribute("data-playing", "true");
-   } else {
-     playButton.innerHTML = "&#9658; Play";
-     container.setAttribute("data-playing", "false");
-   }
- });
-*/
+  playButton.addEventListener("click", () => {
+    if (container.getAttribute("data-playing") === "false") {
+      playButton.innerHTML = "&#10074;&#10074; Pause";
+      container.setAttribute("data-playing", "true");
+    } else {
+      playButton.innerHTML = "&#9658; Play";
+      container.setAttribute("data-playing", "false");
+    }
+  });
+ */
