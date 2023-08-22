befdaae0bea1b1e3751cc8f741723dbf3603441a
Stewart <stilondanga@gmail.com>
2023-08-22 19:54:17 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,13 @@
 
  const playButton = document.getElementById("play-button");
  const container = document.getElementById("button-container");
- /*
-    playButton.addEventListener("click", () => {
-      if (container.getAttribute("data-playing") === "false") {
-        playButton.innerHTML = "&#10074;&#10074; Pause";
-        container.setAttribute("data-playing", "true");
-      } else {
-        playButton.innerHTML = "&#9658; Play";
-        container.setAttribute("data-playing", "false");
-      }
-    });
-   */
+
+ playButton.addEventListener("click", () => {
+   if (container.getAttribute("data-playing") === "false") {
+     playButton.innerHTML = "&#10074;&#10074; Pause";
+     container.setAttribute("data-playing", "true");
+   } else {
+     playButton.innerHTML = "&#9658; Play";
+     container.setAttribute("data-playing", "false");
+   }
+ });
